79c03b4a86d223e5fb93937f79abbffe6c072bed
Sankeerthan D. <UT01235@unicomtic.com>
2024-10-11 11:13:33 +0530

    Question-11

---
+++ Bike.cs
@@ -0,0 +1,9 @@
+﻿namespace BikeRentalManagement_V2;
+
+public class Bike
+{
+    public string BikeId { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public decimal RentalPrice { get; set; }
+}
